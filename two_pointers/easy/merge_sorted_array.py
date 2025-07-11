b6271fc575ee3f39fce55154116229c40023fb85
ayaan2525 <ayaan.siddiqui2525@gmail.com>
2025-07-11 03:06:46 +0200

    problem 14: merge strings alternatively

---
+++ two_pointers/easy/merge_sorted_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternatvely(self, word1: str, word2: str) -> str:
+        merged = []
+        min_len = min(len(word1), len(word2))
+
+        for i in range(min_len):
+            merged.append(word1[i])
+            merged.append(word2[i])
+
+        merged.append(word1[min_len:])
+        merged.append(word2[min_len:])
+
+        return "".join(merged)
