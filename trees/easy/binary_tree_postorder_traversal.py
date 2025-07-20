ac8b67533fc5aa53950e2823e69ef8942a830816
ayaan2525 <ayaan.siddiqui2525@gmail.com>
2025-07-20 15:27:03 +0200

    problem 37: binary-tree-postorder-traversal

--- trees/easy/binary_tree_postorder_traversal.py
+++ trees/easy/binary_tree_postorder_traversal.py
@@ -5,20 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         traverse = []
-        # helper function to perform DFS
+        
         def dfs(node):
             if node:
-                # Traverse the left subtree, visit the node, then traverse the right subtree
+                # Traverse the left subtree, then the right subtree, and finally visit the node
                 dfs(node.left)
-                traverse.append(node.val)
                 dfs(node.right)
+                traverse.append(node.val)
+            return None
         dfs(root)
         return traverse
-
 """
-Question: https://leetcode.com/problems/binary-tree-inorder-traversal/
+Question: https://leetcode.com/problems/binary-tree-postorder-traversal/
 Time Complexity: O(n), where n is the number of nodes in the tree.
-Space Complexity: O(n), for the recursion stack and the output list.
+Space Complexity: O(n), for the recursion stack and the output list. 
 """
